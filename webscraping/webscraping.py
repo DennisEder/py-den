602a4729cdd7bdffbfc45ec725c4ad6c65815e24
Dennis Eder <Dennis.eder91@web.de>
2020-03-31 19:34:54 +0200

    Neue Funktion zum Auslesen der Lenovo-Website  --> Yoga S740

--- webscraping/webscraping.py
+++ webscraping/webscraping.py
@@ -1,1 +1,15 @@
+import typing
+import requests
+from bs4 import BeautifulSoup
 
+pathname: str = 'https://www.lenovo.com/de/de/derewards/laptops/yoga/yoga-s-series/Lenovo-Yoga-S740-15IRH/p/88YGS701211'
+cookies: str
+headers: str
+
+session = requests.session()
+cookies = session.get('https://www.lenovo.com/de/de/pc/')
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0'}
+
+response = session.get(pathname, headers=headers, cookies=cookies)
+soup = BeautifulSoup(response.text, 'html.parser')
+print(response.text)
